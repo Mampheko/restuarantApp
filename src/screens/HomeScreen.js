02287c395f132ccbcad4c7355a81281929acc24d
Tanya <keratilemampheko20@gmail.com>
2022-10-14 16:21:56 +0200

    first commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,20 +11,39 @@ export default function HomeScreen() {
     return (
         <View style={styles.container}>
             <HomeHeader />
-            <ScrollView>
-                <View style={{ marginTop: 10, flexDirection: "row", justifyContent: "space-evenly" }}>
-                    <TouchableOpacity onPress={() => { setDelivery(true) }}>
-                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.buttons : colors.grey5 }}>
-                            <Text style={styles.deliveryText}>Delivery</Text>
-                        </View>
-                    </TouchableOpacity>
+            <ScrollView stickyHeaderIndices={[0]} showsVerticalScrollIndicator={true}>
+                <View>
+                    <View style={{ marginTop: 10, flexDirection: "row", justifyContent: "space-evenly" }}>
+                        <TouchableOpacity onPress={() => { setDelivery(true) }}>
+                            <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.buttons : colors.grey5 }}>
+                                <Text style={styles.deliveryText}>Delivery</Text>
+                            </View>
+                        </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => { setDelivery(false) }}>
-                        <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.grey5 : colors.buttons }}>
-                            <Text style={styles.deliveryText}>Pick Up</Text>
-                        </View>
-                    </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { setDelivery(false) }}>
+                            <View style={{ ...styles.deliveryButton, backgroundColor: delivery ? colors.grey5 : colors.buttons }}>
+                                <Text style={styles.deliveryText}>Pick Up</Text>
+                            </View>
+                        </TouchableOpacity>
+                 65259298298289292   </View>
+                </View>
 
+                <View style={styles.filterview}>
+                    <View style={{ flexDirection: "row", backgroundColor: colors.grey5, borderRadius: 15, paddingVertical: 3 }}>
+                        <View style={{ flexDirection: "row", alignItems: "center", paddingLeft: 10 }}>
+                            <Icon type="material-community" name="map-marker" color={colors.grey1} size={26} />
+                            <Text style={{ marginLeft: 5 }}>16 June street</Text>
+                        </View>
+
+                        <View style={styles.clockview}>
+                            <Icon type="material-community" name="clock-time-four" color={colors.grey1} size={26} />
+                            <Text style={{ marginLeft: 5 }}>Now</Text>
+                        </View>
+                    </View>
+
+                    <View>
+                        <Icon type="material-community" name="tune" color={colors.grey1} size={26} />
+                    </View>
                 </View>
             </ScrollView>
         </View>
@@ -44,6 +63,19 @@ const styles = StyleSheet.create({
     deliveryText: {
         marginLeft: 5,
         fontSize: 16
-    }
+    },
+    filterview: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginHorizontal: 10,
+        marginVertical: 10
+    },
+    clockview:{
+         flexDirection: "row",
+          alignItems: "center",
+           marginLeft: 20,
+           backgroundColor:colors.Cardbackground 
+        }
 })
 
